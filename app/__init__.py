5dda7d9a22f4731debd9fec2fc77d77dfdbadebb
Andy Santana <andysantana@Andys-MacBook-Pro.local>
2023-05-09 23:33:30 -0500

    tusday homwork commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
 #import flask library that we downloaded in the terminal (view in requirements)
 from flask import Flask 
-
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 #makes an instance of our flask library 
 app = Flask(__name__)
-
+app.config.from_object(Config)
+db= SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 from app.blueprints.auth import bp as auth_bp
 app.register_blueprint(auth_bp)
 
 from app.blueprints.main import bp as main_bp
 app.register_blueprint(main_bp)
+
+from app import models
 
--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,4 +1,29 @@
-from flask import render_template
+from flask import render_template, flash,redirect, url_for
 
 from . import bp 
+from app.forms import RegisterForm
+from app.models import User
+from app import db
+@bp.route('/signin')
+def signin():
+    return render_template('signin.jinja')
+
+@bp.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username= form.username.data
+        user=User.query.filter_by(username=username).first()
+        email = User.query.filter_by(email=form.email.data).first()
+        if not email and not user:
+            u= User(username=form.username.data,email=form.email.data,password=form.password.data)
+            u.commit()
+            flash(f'{form.username.data} registered')
+            return redirect(url_for('main.home'))
+        if user:
+            flash(f'{form.username.data} already taken, try again')
+        else:
+            flash(f'{form.email.data} already taken, try again')
+    return render_template('register.jinja',form=form)
+
 
